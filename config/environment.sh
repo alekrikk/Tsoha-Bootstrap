a15f7769bcdb5cd8ba66bf5b2404c00baa476217
Rikkinen Aleksi O <alekrikk@da2-lib-g115-17.helsinki.fi>
2016-01-25 16:01:53 +0200

    Sovellus on alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="alekrikk"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="alekrikk_kansio"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
